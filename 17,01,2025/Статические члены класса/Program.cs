da16800cc22eef02388cda7ffd1b0460ff73dd45
Polina <serysovap@gmail.com>
2025-01-22 00:30:46 +0500

    17.01

---
+++ 17,01,2025/Статические члены класса/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Calculator
+{
+     public static int UsageCount = 0;
+
+    public int Add(int a, int b)
+    {
+        UsageCount++; 
+        return a + b;
+    }
+
+    // Метод для вычитания двух чисел
+    public int Subtract(int a, int b)
+    {
+        UsageCount++; 
+        return a - b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Calculator calculator = new Calculator();
+
+         int sum = calculator.Add(7, 3);
+        Console.WriteLine($"Сумма: {sum}");
+
+        int difference = calculator.Subtract(15, 4);
+        Console.WriteLine($"Разница: {difference}");
+
+        Console.WriteLine($"Количество вызовов методов: {Calculator.UsageCount}");
+    }
+}
