5449467b937e9ed69bd3ba07f3467eb417ee637a
Polina <serysovap@gmail.com>
2025-01-22 00:30:03 +0500

    17.01

---
+++ 17,01,2025/Интерфейсы/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public interface IMovable
+{
+    void Move(int speed);
+}
+
+public class Car : IMovable
+{
+    public void Move(int speed)
+    {
+        Console.WriteLine($"Машина едет со скоростью {speed} км/ч");
+    }
+}
+
+public class Bird : IMovable
+{
+    public void Move(int speed)
+    {
+        Console.WriteLine($"Птица летит со скоростью {speed} км/ч");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IMovable myCar = new Car();
+        IMovable myBird = new Bird();
+
+        myCar.Move(60); 
+        myBird.Move(22); 
+    }
+}
