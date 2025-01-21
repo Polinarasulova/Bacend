517d14c6c1d124040b569a6eaf228ae0c182f636
Polina <serysovap@gmail.com>
2025-01-22 00:31:41 +0500

    21.01(1-3)

---
+++ 21.012025/1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AnimalPoli
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+         public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Годиков: {Age} ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Animal myAnimal = new Animal("Пончик", 18);
+            
+           myAnimal.PrintInfo();
+        }
+    }
+}
---
+++ 21.012025/2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AnimalPolina
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Возраст: {Age}");
+        }
+    }
+
+public class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+         public Dog(string name, int age, string breed) : base(name, age)
+        {
+            Breed = breed;
+        }
+
+        //звук
+        public void Bark()
+        {
+            Console.WriteLine("Гавь!");
+        }
+
+  public new void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Возраст: {Age}, Порода: {Breed}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Вика", 15, "такса");
+            
+            myDog.PrintInfo();
+            
+            myDog.Bark();
+        }
+    }
+}
