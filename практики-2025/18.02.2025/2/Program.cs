05be16c503d28eefc1fbeccb144fa66619b1d80f
Полина Шерышова <serysovap@gmail.com>
2025-03-11 23:06:06 +0500

    исключения с #

---
+++ практики-2025/18.02.2025/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите числитель: ");
+        int numerator = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите знаменатель: ");
+        int denominator = Convert.ToInt32(Console.ReadLine());
+        
+        try
+        {
+            int result = numerator / denominator;
+            Console.WriteLine($"Результат: {result}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Ошибка: деление на ноль недопустимо.");
+        }
+        
+        Console.WriteLine("Программа продолжает работу...");
+    }
+}
