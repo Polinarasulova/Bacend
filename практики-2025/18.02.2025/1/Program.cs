a1cb722ca91da15b358a9ed1fae6d12744907179
Полина Шерышова <serysovap@gmail.com>
2025-03-11 23:15:18 +0500

    исключения с#

---
+++ практики-2025/18.02.2025/1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> data = new Dictionary<string, int>
+        {
+            {"apple", 10},
+            {"banana", 5},
+            {"orange", 7}
+        };
+
+        Console.Write("Введите название фрукта: ");
+        string key = Console.ReadLine();
+
+        try
+        {
+            int value = data[key];
+            Console.WriteLine($"Значение: {value}");
+        }
+        catch (KeyNotFoundException)
+        {
+            Console.WriteLine("Ошибка: введенный ключ не найден в словаре.");
+        }
+
+        Console.WriteLine("Программа продолжает выполнение.");
+    }
+}
