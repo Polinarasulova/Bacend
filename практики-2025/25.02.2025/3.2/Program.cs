8071c7eb4732b6bd663e8b6d57aef370ce6fe0cf
Полина Шерышова <serysovap@gmail.com>
2025-03-11 22:55:40 +0500

    работа с файлами с#

---
+++ практики-2025/25.02.2025/3.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "example.txt";
+        string additionalContent = "Полина крута";
+
+        try
+        {
+            File.AppendAllText(path, additionalContent + Environment.NewLine);
+            Console.WriteLine("Текст добавлен в файл.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
