46b762318a534762e1c9f44ef59dd8ae1f5e49ee
Полина Шерышова <serysovap@gmail.com>
2025-03-11 23:14:04 +0500

    исключения с#

---
+++ практики-2025/18.02.2025/3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, int> data = new Dictionary<string, int>
+        {
+            { "apple", 10 },
+            { "banana", 5 },
+            { "orange", 7 }
+        };
+
+        try
+        {
+            Console.Write("Введите название фрукта: ");
+            string key = Console.ReadLine();
+            if (data.ContainsKey(key))
+            {
+                Console.WriteLine($"Значение: {data[key]}"); 
+            }
+            else
+            {
+                throw new KeyNotFoundException("Ключ не найден в словаре.");
+            }
+
+            Console.Write("Введите числитель: ");
+            if (int.TryParse(Console.ReadLine(), out int numerator)) 
+            {
+                Console.WriteLine($"Числитель: {numerator}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено не число.");
+            }
+        }
+        catch (KeyNotFoundException ex)
+        {
+            Console.WriteLine(ex.Message); 
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Произошла ошибка: {ex.Message}"); 
+        }
+    }
+}
