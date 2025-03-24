2156e0f62a7463cd9d59988ecabb615608390306
Полина Шерышова <serysovap@gmail.com>
2025-03-24 23:23:31 +0500

    API.GET I POST

---
+++ практики-2025/API.GET I POST/A/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            try
+            {
+                string url = "https://jsonplaceholder.typicode.com/users/1";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine("Ответ: ");
+                    Console.WriteLine(responseBody);
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при запросе: {response.StatusCode}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" Oшибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ практики-2025/API.GET I POST/B/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Console.WriteLine("Введите заголовок : ");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Введите текст : ");
+        string body = Console.ReadLine();
+
+        Console.WriteLine("Введите ID пользователя : ");
+        int userId = int.Parse(Console.ReadLine());
+
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            // JSON-строки
+            string json = $"{{\"title\":\"{title}\", \"body\":\"{body}\", \"userId\":{userId}}}";
+
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            //POST-запрос
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            
+            if (response.IsSuccessStatusCode)
+            {
+               string result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Данные успешно отправлены : ");
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine($"Ошибка при отправке данных : {response.StatusCode}");
+            }
+        }
+    }
+}
